3a7a325a2d475f764685970d0e0cf933f4a1b1f0
LinoGitas <63246130+LinoGitas@users.noreply.github.com>
2020-04-27 14:11:20 +0300

    Initial commit and variable init

---
+++ js/variables.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/* 
+kintamuju tipai:
+- const
+- let
+- var
+*/
+
+const lastName = 'Rimantas';            //tekstas
+const akiuSkaicius = 2;                 //skaicius
+const turiGalva = true;                 //logine reiksme true/false
+const mokaSkraidyti = false;            //logine reiksme true/
+const petriukoPazymiai = [];            //tuscias sarasas
+const marytesPazymiai = [10, 8, 2];     //sarasas su skaiciais
+const abc = ['a', 'b', 'c', 'd', 'e'];  //sarasas su tekstu
+
+console.log( lastName );
+console.log( akiuSkaicius );
+console.log( turiGalva );
+console.log( mokaSkraidyti );
+console.log( petriukoPazymiai );
+console.log( marytesPazymiai );
+console.log( abc );
+
+
+let megstamasSkaicius = 5;
+console.log( 'Megstamas skaicius:', megstamasSkaicius  );
+
+megstamasSkaicius = 3;
+console.log( 'Megstamas skaicius:', megstamasSkaicius  );
+
+megstamasSkaicius = 7;
+console.log( 'Megstamas skaicius:', megstamasSkaicius  );
+
+const belekoksSarasas = [5, 'abc', true, false, [18, '-asfg',[]]];
+console.log( belekoksSarasas );
